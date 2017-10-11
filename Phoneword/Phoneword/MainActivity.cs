6748323313faef8a27a412e6ac8c5dc6b0107885
Omar <omarjavier15@gmail.com>
2017-10-11 15:37:40 -0400

    Add phone world

---
+++ Phoneword/Phoneword/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using static System.String;
+using Uri = Android.Net.Uri;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+
+            var translatedNumber = Empty;
+
+            translateButton.Click += (sender, e) =>
+            {
+                translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+
+                if (IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = $"Call #{translatedNumber}";
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (sender, args) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+
+                callDialog.SetMessage($"Call #{translatedNumber}?");
+
+                callDialog.SetNeutralButton("Call", (o, eventArgs) =>
+                {
+                    var callIntent = new Intent(Intent.ActionCall);
+
+                    callIntent.SetData(Uri.Parse("tel:" + translatedNumber));
+
+                    StartActivity(callIntent);
+                });
+
+                callDialog.SetNegativeButton("Cancel", (o, eventArgs) => { });
+
+                callDialog.Show();
+            };
+        }
+    }
+}
+
---
+++ Phoneword/README.md
@@ -0,0 +1,4 @@
+# Phoneword
+Xamarin tutorial
+
+https://developer.xamarin.com/guides/android/getting_started/hello,android/hello,android_quickstart/
