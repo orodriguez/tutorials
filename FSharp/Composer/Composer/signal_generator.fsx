aa6ef66745a9f2b6f6fe4b325c6b97db27fde472
Omar <omarjavier15@gmail.com>
2017-11-18 17:24:31 -0400

    Create Composer project

---
+++ FSharp/Composer/Composer/signal_generator.fsx
@@ -0,0 +1,15 @@
+﻿let generateSamples milliseconds frequency =
+  let sampleRate = 44100.
+  let sixteenBitSampleLimit = 327667.
+  let volume = 0.8
+  let toAmplitude x = 
+    x
+    |> (*) (2. * System.Math.PI * frequency / sampleRate)
+    |> sin
+    |> (*) sixteenBitSampleLimit
+    |> (*) volume
+    |> int16
+  
+  let numOfSamples = milliseconds / 1000. * sampleRate
+  let requiredSamples = seq { 1.0..numOfSamples }
+  Seq.map toAmplitude requiredSamples
