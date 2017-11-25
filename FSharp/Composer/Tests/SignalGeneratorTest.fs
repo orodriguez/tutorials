7a220cc0cb348fafd119e5b5f1e4eda2ad33d57c
Omar <omarjavier15@gmail.com>
2017-11-24 21:12:59 -0400

    Add more tests

--- FSharp/Composer/Tests/SignalGeneratorTest.fs
+++ FSharp/Composer/Tests/SignalGeneratorTest.fs
@@ -9,3 +9,17 @@ type ``When generating 2 seconds at 440Hz`` ()=
   member this.``there should be 88200 samples`` ()=
     let samples = generateSamples 2000. 440.
     Assert.Equal(88200, Seq.length samples)
+  
+  [<Fact>]
+  member this.``all samples should be in range`` ()=
+    let limit = 32767s
+    let samples = generateSamples 2000. 440.
+    samples |> Seq.iter (fun s -> Assert.True(s > (-1s * limit)))
+
+type ``When generating 2 seconds at 0Hs`` ()=
+  
+  [<Fact>]
+  member this.``the samples should all be 0`` ()=
+    let samples = generateSamples 2000. 0.
+    let expected = Seq.init 88200 (fun i -> int16 0)
+    Assert.Equal<int16>(expected,  samples)
